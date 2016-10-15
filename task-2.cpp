d5dde9421b3a661b64667f0f27257d25efe988c9
tempname <vklokun@protonmail.ch>
2016-10-15 19:48:36 +0300

    Task 2: Public and private access specifiers.

---
+++ task-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class DateClass
+{
+	int m_month;
+	int m_day;
+	int m_year;
+public:
+	void setDate(int day, int month, int year)
+	{
+		m_day = day;
+		m_month = month;
+		m_year = year;
+	}
+	
+	void print()
+	{
+		std::cout << m_day << "." << m_month << "." << m_year << "." << std::endl;
+	}
+};
+
+int main()
+{
+	DateClass date;
+	date.setDate(16,10,2016);
+	date.print();
+
+	return 0;
+}
